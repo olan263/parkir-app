80c45bbcc44714320c4ca083738c050ce10137a1
Olan <olanrina9@gmail.com>
2025-12-22 17:07:27 +0700

    Fixing storage path and enabling error display

--- api/index.php
+++ api/index.php
@@ -1,8 +1,12 @@
 <?php
 
-// Paksa agar error tampil di layar
+// PAKSA TAMPILKAN ERROR
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Pastikan autoload terbaca
+require __DIR__ . '/../vendor/autoload.php';
+
+// Jalankan aplikasi
 require __DIR__ . '/../public/index.php';
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,23 +3,27 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-public function register(): void
-{
-    if (env('APP_ENV') === 'production') {
-        $this->app->useStoragePath('/tmp');
+    public function register(): void
+    {
+        // Mengarahkan folder storage ke /tmp karena Vercel bersifat read-only
+        if (config('app.env') === 'production') {
+            $this->app->useStoragePath('/tmp');
+        }
     }
-}
+
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        // Menetapkan default string length untuk menghindari error index mysql
+        Schema::defaultStringLength(191);
     }
 }
